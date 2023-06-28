cdcd990f20556b0bdc7c21cd6a7679941e4d61a0
JoKrus <JoKrus@web.de>
2023-06-28 12:32:17 +0200

    fixed mount disable added itemframe prevention

--- src/main/java/net/jcom/minecraft/battleplugin/handler/LobbyHandler.java
+++ src/main/java/net/jcom/minecraft/battleplugin/handler/LobbyHandler.java
@@ -2,12 +2,17 @@ package net.jcom.minecraft.battleplugin.handler;
 
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.ItemFrame;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.plugin.Plugin;
 import org.spigotmc.event.entity.EntityMountEvent;
 
@@ -47,12 +52,45 @@ public class LobbyHandler implements Listener {
 
     @EventHandler
     public void onHorseMount(EntityMountEvent entityMountEvent) {
-        if (!(entityMountEvent.getEntity() instanceof Player p)) {
+        if (!(entityMountEvent.getMount() instanceof Player p)) {
             return;
         }
 
         if (p.getGameMode() == GameMode.ADVENTURE) {
             entityMountEvent.setCancelled(true);
+        }
+    }
+
+
+    @EventHandler
+    public void frameDrop(EntityDamageByEntityEvent e) {
+        if (!(e.getDamager() instanceof Player p)) {
+            return;
+        }
+
+        if (p.getGameMode() == GameMode.ADVENTURE) {
+            if (e.getEntity() instanceof ItemFrame) {
+                if (e.getDamager() instanceof Player) {
+                    e.setCancelled(true);
+
+                }
+                if (e.getDamager() instanceof Projectile) {
+                    if (((Projectile) e.getDamager()).getShooter() instanceof Player) {
+                        e.getDamager().remove();
+                        e.setCancelled(true);
+                    }
+                }
+            }
+        }
+    }
+
+
+    @EventHandler
+    public void frameRotate(PlayerInteractEntityEvent e) {
+        if (e.getPlayer().getGameMode() == GameMode.ADVENTURE) {
+            if (e.getRightClicked().getType().equals(EntityType.ITEM_FRAME)) {
+                e.setCancelled(true);
+            }
         }
     }
 }
